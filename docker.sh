c384658e09f8347b0ffef4ff77bf568684659c93
iwconfig <snelhingst@gmail.com>
2025-03-18 13:32:17 +0000

    test

--- docker.sh
+++ docker.sh
@@ -5,11 +5,7 @@ docker run --rm -it -v /workspaces/jubilant-octo-giggle:/workspaces:rw debian:si
     apt clean && \
     curl -sfLo /etc/apt/trusted.gpg.d/jubilant-octo-giggle-keyring.asc https://iwconfig.github.io/jubilant-octo-giggle/gpg.key && \
     printf '%s https://iwconfig.github.io/jubilant-octo-giggle/ sid main\n' deb deb-src >/etc/apt/sources.list.d/jubilant-octo-giggle.list && \
-    cat << EOF > /etc/apt/preferences.d/jubilant-octo-giggle && \
-    Package: *
-    Pin: origin iwconfig.github.io
-    Pin-Priority: 1001
-    EOF && \
+    printf 'Package: *\nPin: %s %s\nPin-Priority: %s\n' origin iwconfig.github.io 1001 > /etc/apt/preferences.d/jubilant-octo-giggle && \
     apt update && \
     exec bash
 "
